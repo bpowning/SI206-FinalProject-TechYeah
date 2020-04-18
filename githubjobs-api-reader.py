6244fe394ea58226b35a12bbf8a5625dfe6198af
Brooke Powning <bpowning@umich.edu>
2020-04-18 12:05:28 -0400

    working api call

--- githubjobs-api-reader.py
+++ githubjobs-api-reader.py
@@ -2,46 +2,54 @@ import sqlite3
 import json
 import os
 import requests
+import urllib.request, urllib.parse, urllib.error
 
 # https://jobs.github.com/api
 
 # get directory path and create cache file
-dir_path = os.path.dirname(os.path.realpath(__file__))
-CACHE_FNAME = dir_path + '/' + "cache_jobs.json"
+path = os.path.dirname(os.path.realpath(__file__))
+jobs_file = path + '/' + "cache_jobs.json"
 
 # if there is data in the cache file, load the content into a dictionary or leave it empty
-try:
-    cache_file = open(CACHE_FNAME, 'r', encoding="utf-8") # Try to read the data from the file
-    cache_contents = cache_file.read()  # If it's there, get it into a string
-    CACHE_DICTION = json.loads(cache_contents) # And then load it into a dictionary
-    cache_file.close() # Close the file, we're good, we got the data in a dictionary.
-except:
-    CACHE_DICTION = {}
+def read_cache(cache_file):
+    try:
+        file = open(cache_file, 'r')
+        cache_dict = json.loads(file.read())
+        file.close()
+        return cache_dict
+    except:
+        cache_dict = {}
+        return cache_dict
 
 # writes the JSON to the cache file and saves the results
-path_to_file = os.path.join(os.path.dirname(__file__), CACHE_FNAME)
-with open(path_to_file, 'w') as f:
-    x = json.dumps(CACHE_DICTION)
-    f.write(json.dumps(CACHE_DICTION))
+def write_cache(cache_file, cache_dict):
+    file_path = os.path.join(os.path.dirname(__file__), cache_file)
+    file = open(file_path, 'w')
+    file.write(json.dumps(cache_dict))
 
-<<<<<<< HEAD
-# function to create a url based on the different parameters you want to search
-def create_base_url(description = "", location = "", lat = "", long = "", full_time = ""):
-    base_url = "https://jobs.github.com/positions?" + 
-
-=======
 # functions to create a url based on the different parameters you want to search
 def url_by_location(location):
-    base_url = "https://jobs.github.com/positions?" + "location=" + location
+    base_url = "https://jobs.github.com/positions.json?" + "location=" + location
     return base_url
 
 def url_by_jobtype(description):
-    base_url = "https://jobs.github.com/positions?" + "description=" + description
+    base_url = "https://jobs.github.com/positions.json?" + "description=" + description
     return base_url
 
 def url_by_both(location, description):
-    base_url = "https://jobs.github.com/positions?" + "location=" + location + "&description=" + description
+    base_url = "https://jobs.github.com/positions.json?" + "location=" + location + "&description=" + description
     return base_url
 
-# function to get data from an API call
->>>>>>> 8797668f814c803182516e441d830283cece0eeb
+# get data from an API call
+def get_data(base_url, cache_file = jobs_file):
+    cache_dict = read_cache(cache_file)
+    if base_url in cache_dict:
+        return cache_dict[base_url]
+    else:
+        request = requests.get(base_url) 
+        cache_dict[base_url] = json.loads(request.text)
+        write_cache(cache_file, cache_dict)
+        return cache_dict[base_url]
+
+url = url_by_location("newyork")
+get_data(url)
